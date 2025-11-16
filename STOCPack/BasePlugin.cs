ea3a2c98e5a31c606da0ccdc1926e7340be46750
Elidiane <121459020+elidianesampaiotrabuco@users.noreply.github.com>
2025-11-16 19:26:13 -0300

    Add project files.

---
+++ STOCPack/BasePlugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+
+using HarmonyLib;
+
+using MTM101BaldAPI;
+
+namespace STOCPack
+{
+    [BepInPlugin("starrie.bbplus.stoc", "Shaldi's Tower of Chaos Pack", "0.0.1")]
+
+    [BepInDependency("mtm101.rulerp.bbplus.baldidevapi")]
+
+    public class BasePlugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Harmony harmony = new Harmony("starrie.bbplus.stoc");
+
+            harmony.PatchAll();
+        }
+    }
+}
