84766b8757368aff6d30239537c324d293c9f73b
Elidiane <121459020+elidianesampaiotrabuco@users.noreply.github.com>
2025-11-17 02:11:07 -0300

    final touches before snapshot 2 release
    the krabby patty item was planned to be added, but was scrapped due to a bug that broke the entire mod.

--- STOCPack/BasePlugin.cs
+++ STOCPack/BasePlugin.cs
@@ -6,6 +6,7 @@ using MTM101BaldAPI.ObjectCreation;
 using MTM101BaldAPI.Registers;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace STOCPack
@@ -19,6 +20,8 @@ namespace STOCPack
         public AssetManager assetMan = new AssetManager();
         private static string npcSubDirectory = "Textures/NPCs";
         private static string npcAudioSubDirectory = "Audio/NPCs";
+        private static string itemSubDirectory = "Textures/Items";
+        private static string itemAudioSubDirectory = "Audio/Items";
         public void Awake()
         {
             Harmony harmony = new Harmony("starrie.bbplus.stoc");
@@ -56,6 +59,20 @@ namespace STOCPack
             .SetPoster(assetMan.Get<Texture2D>("Stroketon_Poster"), "Stroketon", "Dr. Jr.")
             .Build();
 
+            yield return "Building items...";
+
+            ItemObject krabbyPatty = new ItemBuilder(Info)
+    .SetNameAndDescription("Krabby Patty", "TBA")
+    .SetSprites(assetMan.Get<Sprite>("KrabbyPattySmallSprite"), assetMan.Get<Sprite>("KrabbyPattyLargeSprite"))
+    .SetEnum("KrabbyPatty")
+    .SetShopPrice(350)
+    .SetGeneratorCost(40)
+    .SetItemComponent<ITM_KrabbyPatty>()
+    .SetMeta(ItemFlags.Persists, new string[] { "food" })
+    .Build();
+            //((ITM_KrabbyPatty)krabbyPatty.item).eatSound = (SoundObject)((ITM_ZestyBar)ItemMetaStorage.Instance.FindByEnum(Items.ZestyBar).value.item).ReflectionGetVariable("audEat");
+            assetMan.Add<ItemObject>("KrabbyPatty", krabbyPatty);
+
             yield return "Doing some miscellaneous stuff...";
 
             PinkGuy.shaldiSprite = assetMan.Get<Sprite>("ShaldiSprite");
@@ -76,6 +93,11 @@ namespace STOCPack
             assetMan.Add<Texture2D>("Stroketon_Poster", AssetLoader.TextureFromMod(this, npcSubDirectory, "Stroketon/Stroketon_Poster.png"));
             assetMan.Add<Sprite>("StroketonSprite", AssetLoader.SpriteFromTexture2D(assetMan.Get<Texture2D>("Stroketon"), 50));
             assetMan.Add<SoundObject>("StroketonDrJr", ObjectCreators.CreateSoundObject(AssetLoader.AudioClipFromMod(this, npcAudioSubDirectory, "Stroketon/STRO_DrJr.wav"), "Dr. Jr.", SoundType.Music, Color.green));
+            assetMan.Add<SoundObject>("KrabbyPattyCrunch", ObjectCreators.CreateSoundObject(AssetLoader.AudioClipFromMod(this, itemAudioSubDirectory, "KrabbyPatty/Crunch.wav"), "*CRUNCH*", SoundType.Effect, Color.white));
+            assetMan.Add<Texture2D>("KrabbyPattySmall", AssetLoader.TextureFromMod(this, itemSubDirectory, "KrabbyPatty/KrabbyPattyIcon_Small.png"));
+            assetMan.Add<Texture2D>("KrabbyPattyLarge", AssetLoader.TextureFromMod(this, itemSubDirectory, "KrabbyPatty/KrabbyPattyIcon_Large.png"));
+            assetMan.Add<Sprite>("KrabbyPattySmallSprite", AssetLoader.SpriteFromTexture2D(assetMan.Get<Texture2D>("KrabbyPattySmall"), 50));
+            assetMan.Add<Sprite>("KrabbyPattyLargeSprite", AssetLoader.SpriteFromTexture2D(assetMan.Get<Texture2D>("KrabbyPattyLarge"), 50));
         }
 
         private void AddObjects(string floor, int floorNumber, SceneObject floorObject)
@@ -94,6 +116,7 @@ namespace STOCPack
                     weight = floorNumber < 2 ? 150 * floorNumber : 475
                 }
                 );
+                //floorObject.levelObject.potentialItems = floorObject.levelObject.potentialItems.AddItem(new WeightedItemObject() { selection = assetMan.Get<ItemObject>("KrabbyPatty"), weight = 100 }).ToArray(); - maybe commenting that out would fix it
             }
             else if (floor == "END")
             {
@@ -109,7 +132,10 @@ namespace STOCPack
                     weight = 475
                 }
                 );
+                //floorObject.levelObject.potentialItems = floorObject.levelObject.potentialItems.AddItem(new WeightedItemObject() { selection = assetMan.Get<ItemObject>("KrabbyPatty"), weight = 100 }).ToArray(); - ditto
             }
+
+            //floorObject.shopItems = floorObject.shopItems.AddItem(new WeightedItemObject() { selection = assetMan.Get<ItemObject>("KrabbyPatty"), weight = 100 }).ToArray(); - the mod is gonna focus on adding npcs, for now
 
             floorObject.MarkAsNeverUnload();
         }
---
+++ STOCPack/ITM_KrabbyPatty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace STOCPack
+{
+    public class ITM_KrabbyPatty : Item
+    {
+        public SoundObject KrabbyPattyCrunch;
+
+        public override bool Use(PlayerManager pm)
+        {
+            pm.plm.stamina = pm.plm.staminaMax * 2f;
+            Singleton<CoreGameManager>.Instance.audMan.PlaySingle(KrabbyPattyCrunch);
+            return true;
+        }
+    }
+}
