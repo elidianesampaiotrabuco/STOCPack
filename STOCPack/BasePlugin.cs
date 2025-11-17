c8bfdd7129089faa896a4fc3396b0ec911fbcf5f
Elidiane <121459020+elidianesampaiotrabuco@users.noreply.github.com>
2025-11-16 21:32:22 -0300

    added shaldi himself

--- STOCPack/BasePlugin.cs
+++ STOCPack/BasePlugin.cs
@@ -1,8 +1,12 @@
 ﻿using BepInEx;
-
 using HarmonyLib;
-
 using MTM101BaldAPI;
+using MTM101BaldAPI.AssetTools;
+using MTM101BaldAPI.ObjectCreation;
+using MTM101BaldAPI.Registers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace STOCPack
 {
@@ -12,11 +16,73 @@ namespace STOCPack
 
     public class BasePlugin : BaseUnityPlugin
     {
+        public AssetManager assetMan = new AssetManager();
+        private static string npcSubDirectory = "Textures/NPCs";
         public void Awake()
         {
             Harmony harmony = new Harmony("starrie.bbplus.stoc");
 
             harmony.PatchAll();
+
+            GetAssets();
+
+            LoadingEvents.RegisterOnAssetsLoaded(Info, RegisterAssets(), LoadingEventOrder.Pre);
+            GeneratorManagement.Register(this, GenerationModType.Addend, AddObjects);
+        }
+
+        private IEnumerator RegisterAssets()
+        {
+            yield return 1;
+
+            yield return "Building NPCs...";
+
+            Shaldi PinkGuy = new NPCBuilder<Shaldi>(Info)
+            .SetName("Shaldi")
+            .SetEnum("ThatPinkGuy")
+            .IgnorePlayerOnSpawn()
+            .AddLooker()
+            .AddTrigger()
+            .AddSpawnableRoomCategories(new RoomCategory[] { RoomCategory.Hall })
+            .SetPoster(assetMan.Get<Texture2D>("Shaldi_Poster"), "Shaldi", "Do not let him catch you.")
+            .Build();
+
+            yield return "Doing some miscellaneous stuff...";
+
+            PinkGuy.shaldiSprite = assetMan.Get<Sprite>("ShaldiSprite");
+
+            assetMan.Add<NPC>("Shaldi", PinkGuy);
+
+        }
+
+        private void GetAssets()
+        {
+            assetMan.Add<Texture2D>("Shaldi", AssetLoader.TextureFromMod(this, npcSubDirectory, "Shaldi.png"));
+            assetMan.Add<Texture2D>("Shaldi_Poster", AssetLoader.TextureFromMod(this, npcSubDirectory, "Shaldi_Poster.png"));
+            assetMan.Add<Sprite>("ShaldiSprite", AssetLoader.SpriteFromTexture2D(assetMan.Get<Texture2D>("Shaldi"), 50));
+        }
+
+        private void AddObjects(string floor, int floorNumber, SceneObject floorObject)
+        {
+            if (floor.StartsWith("F"))
+            {
+                floorObject.potentialNPCs.Add(new WeightedNPC()
+                {
+                    selection = assetMan.Get<NPC>("Shaldi"),
+                    weight = floorNumber < 2 ? 100 * floorNumber : 250
+                }
+                );
+            }
+            else if (floor == "END")
+            {
+                floorObject.potentialNPCs.Add(new WeightedNPC()
+                {
+                    selection = assetMan.Get<NPC>("Shaldi"),
+                    weight = 250
+                }
+                );
+            }
+
+            floorObject.MarkAsNeverUnload();
         }
     }
 }
---
+++ STOCPack/Shaldi.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace STOCPack
+{
+    public class Shaldi : NPC
+    {
+        public Sprite shaldiSprite;
+
+        public float WanderSpeed = 2f;
+
+        //private bool caught;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            spriteRenderer[0].sprite = shaldiSprite;
+
+            behaviorStateMachine.ChangeState(new Shaldi_Wander(this));
+
+            base.Navigator.SetSpeed(this.WanderSpeed);
+
+            base.Navigator.maxSpeed = this.WanderSpeed;
+        }
+
+        public void StartPersuingPlayer(PlayerManager player)
+        {
+            behaviorStateMachine.ChangeNavigationState(new NavigationState_TargetPlayer(this, 63, player.transform.position));
+        }
+        public void PersuePlayer(PlayerManager player)
+        {
+            behaviorStateMachine.CurrentNavigationState.UpdatePosition(player.transform.position);
+        }
+    }
+
+    public class Shaldi_StateBase : NpcState
+    {
+        protected Shaldi PinkGuy;
+
+        public Shaldi_StateBase(Shaldi shald) : base(shald)
+        {
+            PinkGuy = shald;
+        }
+    }
+
+    public class Shaldi_Wander : Shaldi_StateBase
+    {
+        private bool caught;
+
+        public Shaldi_Wander(Shaldi shald) : base(shald)
+        {
+
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            if (!npc.Navigator.HasDestination)
+            {
+                ChangeNavigationState(new NavigationState_WanderRandom(npc, 0));
+            }
+        }
+
+        public override void OnStateTriggerEnter(Collider other, bool validCollision)
+        {
+            base.OnStateTriggerEnter(other, validCollision);
+            PlayerManager component = other.GetComponent<PlayerManager>();
+            bool flag = validCollision && component != null && !this.caught;
+            if (flag)
+            {
+                Baldi baldi = this.npc.ec.GetBaldi();
+                bool flag2 = baldi == null;
+                if (flag2)
+                {
+                    this.npc.ec.SpawnNPC(Resources.FindObjectsOfTypeAll<NPC>().First((NPC x) => x.name == "Baldi"), default(IntVector2));
+                    baldi = this.npc.ec.GetBaldi();
+                }
+                this.npc.Entity.SetHeight(4f);
+                this.npc.gameObject.transform.LookAt(component.transform);
+                baldi.transform.position = this.npc.gameObject.transform.position;
+                baldi.Entity.SetHeight(-5f);
+                Singleton<CoreGameManager>.Instance.EndGame(component.transform, baldi);
+                this.caught = true;
+            }
+        }
+
+        public override void DestinationEmpty()
+        {
+            base.DestinationEmpty();
+            ChangeNavigationState(new NavigationState_WanderRandom(npc, 0));
+        }
+
+        public override void PlayerSighted(PlayerManager player)
+        {
+            base.PlayerSighted(player);
+            if (!player.Tagged)
+            {
+                PinkGuy.StartPersuingPlayer(player);
+            }
+        }
+
+        public override void PlayerInSight(PlayerManager player)
+        {
+            base.PlayerInSight(player);
+            if (!player.Tagged)
+            {
+                PinkGuy.PersuePlayer(player);
+            }
+        }
+    }
+}
